a3a6057e51b234552c561d3227e9cd5264f41e5e
Anton Khomenko <regiysa@gmail.com>
2023-09-06 17:06:04 +0300

    chore: front apollo client implementation

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,15 +2,19 @@ import {SessionProvider} from "next-auth/react";
 import type {AppProps} from "next/app";
 import {ChakraProvider} from "@chakra-ui/react";
 import {theme} from "@/chakra/theme";
+import {ApolloProvider} from "@apollo/client";
+import {client} from "@/graphql/apollo-client";
 
 
 function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
-            <SessionProvider session={session}>
-                <Component {...pageProps} />
-            </SessionProvider>
-        </ChakraProvider>
+        <ApolloProvider client={client}>
+            <ChakraProvider theme={theme}>
+                <SessionProvider session={session}>
+                    <Component {...pageProps} />
+                </SessionProvider>
+            </ChakraProvider>
+        </ApolloProvider>
     );
 }
 
